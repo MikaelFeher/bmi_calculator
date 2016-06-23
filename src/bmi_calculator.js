278a7ad6c7ee593bd7b02b3a116599051e929b91
MikaelFeher <mikael.feher@gmail.com>
2016-06-23 10:46:01 +0200

    added function for calculating imperial values in the bmi_calculator.js

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -11,24 +11,24 @@ BMICalculator.prototype.metric_bmi = function(obj) {
   }
 };
 
-// BMICalculator.prototype.imperial_bmi = function(obj) {
-//   var weight = obj.weight;
-//   var height = obj.height;
-//   if (weight > 0 && height > 0) {
-//     var finalBmi = weight / (height / 100 * height / 100);
-//     obj.bmiValue = parseFloat(finalBmi.toFixed(2));
-//     setBMIMessage(obj);
-//   }
-// };
+BMICalculator.prototype.imperial_bmi = function(obj) {
+  var weight = obj.weight;
+  var height = obj.height;
+  if (weight > 0 && height > 0) {
+    var finalBmi = (weight * 703) / (height * height);
+    obj.bmiValue = parseFloat(finalBmi.toFixed(2));
+    setBMIMessage(obj);
+  }
+};
 
 function setBMIMessage (obj, value) {
-  if (obj.bmiValue < 18.5){
+  if (obj.bmiValue <= 18.5){
     obj.bmiMessage = "Underweight"
   }
-  if (obj.bmiValue > 18.5 && obj.bmiValue < 25){
+  if (obj.bmiValue > 18.5 && obj.bmiValue <= 25){
     obj.bmiMessage = "Normal"
   }
-  if (obj.bmiValue > 25 && obj.bmiValue < 30) {
+  if (obj.bmiValue > 25 && obj.bmiValue <= 30) {
     obj.bmiMessage = "Overweight"
   }
   if (obj.bmiValue > 30) {
